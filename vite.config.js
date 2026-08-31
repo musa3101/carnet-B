a8c51ddcd2c4cb7bdf21e76e30b446afdbb7eb23
musa3101 <musa3101@users.noreply.github.com>
2026-08-31 19:15:25 +0200

    fix(cloudflare): configure SPA routing with _redirects and _headers and Safari 14 build target

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
+    chunkSizeWarningLimit: 2000,
+    cssCodeSplit: false
+  },
   server: {
     port: 5173,
     host: true
